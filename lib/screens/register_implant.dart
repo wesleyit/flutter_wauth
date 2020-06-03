eb3aa44d1872e52bc562dd11295cdfdb3f135002
Wesley Rodrigues <wesrod@amazon.com>
2020-06-02 22:19:14 -0300

    Improve the Screen UX
    The page had problems with margins, and the button was too small. These
    fixes will improve the overall usability.

--- lib/screens/register_implant.dart
+++ lib/screens/register_implant.dart
@@ -13,35 +13,53 @@ class RegisterImplantScreen extends StatelessWidget {
               style: t.textTheme.bodyText1,
               child: Container(
                   child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Image.asset('assets/images/padlock.png'),
-                  SizedBox(height: 30.0),
-                  TextField(
-                      style: t.textTheme.bodyText1,
-                      decoration: InputDecoration(hintText: 'Implant name')),
-                  SizedBox(height: 30.0),
-                  TextField(
-                      style: t.textTheme.bodyText1,
-                      decoration: InputDecoration(hintText: 'PIN (4 digits)'),
-                      obscureText: true),
-                  SizedBox(height: 45.0),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.wifi,
-                          color: myYellow,
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(height: 30.0),
+                        TextField(
+                            style: t.textTheme.bodyText1,
+                            decoration:
+                                InputDecoration(hintText: 'Implant name')),
+                        SizedBox(height: 30.0),
+                        TextField(
+                            keyboardType: TextInputType.number,
+                            style: t.textTheme.bodyText1,
+                            decoration:
+                                InputDecoration(hintText: 'PIN (4 digits)'),
+                            obscureText: true),
+                        SizedBox(height: 45.0),
+                        RaisedButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30.0, vertical: 10.0),
+                          splashColor: myYellow,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.wifi,
+                                color: myYellow,
+                              ),
+                              Text('  Scan!', style: t.textTheme.headline2)
+                            ],
+                          ),
                         ),
-                        Text('  Scan!', style: t.textTheme.headline2)
+                        Padding(
+                            padding: EdgeInsets.only(
+                                bottom:
+                                    MediaQuery.of(context).viewInsets.bottom))
                       ],
                     ),
                   ),
-                  Padding(
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom))
                 ],
               ))),
         ));
