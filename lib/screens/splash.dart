972b55cb87e6a3c3b5cb71410a46dbdca19dd32b
Wesley Rodrigues <wesrod@amazon.com>
2020-06-05 01:35:18 -0300

    Add File Storage Support
    This commit adds file storage support via path_provider plugin. It saves
    a file and checks during the Splash Screen.

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(context) {
-    Timer(Duration(seconds: 3),
-        () => Navigator.of(context).pushNamed('/RegisterImplant'));
+    Timer(Duration(seconds: 3), () async {
+      try {
+        final directory = await getApplicationDocumentsDirectory();
+        final file = File('${directory.path}/my_file.txt');
+        String text = await file.readAsString();
+        print('Profile found with: ' + text);
+        Navigator.of(context).pushNamed('/AccountList');
+      } catch (e) {
+        print('Profile not found!');
+        Navigator.of(context).pushNamed('/RegisterImplant');
+      }
+    });
     var t = Theme.of(context);
     return Scaffold(
         backgroundColor: t.primaryColor,
